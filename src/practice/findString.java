5aa7431ab36d4234e7b316e436f2b903b57c5dbe
BangKiHyun <rlrlvh@naver.com>
2019-06-15 00:18:26 +0900

    단순 문자열 찾기 수정

--- src/practice/findString.java
+++ src/practice/findString.java
@@ -4,31 +4,36 @@ import java.util.Scanner;
 
 public class findString {
 
-    static int find(String parent, String pattern){
-        int parenSize = parent.length();
+    static int find(String parent, String pattern) {
+        int parentSize = parent.length();
         int patternSize = pattern.length();
-        for(int i=0;i<=parenSize - parenSize; i++){
+        char[] str = parent.toCharArray();
+        char[] compare = pattern.toCharArray();
+
+        for (int i = 0; i <= parentSize - patternSize; i++) {
             boolean finded = true;
-            for(int j=0; j<patternSize; j++){
-                if(parent.substring() != pattern[j]){
+            for (int j = 0; j < patternSize; j++) {
+                if (str[i + j] != compare[j]) {
                     finded = false;
                     break;
                 }
-            }
-            if(finded){
-                return i;
+                if (finded) {
+                    return i;
+                }
             }
         }
         return -1;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
         String pattern = sc.next();
-
         int result = find(input, pattern);
-
-        System.out.print(result + "번째에서 일치");
+        if (result == -1) {
+            System.out.println("값을 찾을 수 없습니다");
+        } else {
+            System.out.print(result + "번째에서 찾았습니다");
+        }
     }
 }
