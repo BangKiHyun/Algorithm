ce0970442e982e50d12df3ed61fcbbfdaebdea91
BangKiHyun <rlrlvh@naver.com>
2019-07-04 18:33:24 +0900

    Queue

---
+++ src/programmers/pg_42587.java
@@ -0,0 +1,36 @@
+package programmers;
+
+import java.util.*;
+
+public class pg_42587 {
+    static public int solution(int[] priorities, int location) {
+        int answer = 1;
+
+        PriorityQueue priority = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int task : priorities){
+            priority.add(task);
+        }
+        while(!priority.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(priorities[i] == (int)priority.peek()) {
+                    if(i == location){
+                        return answer;
+                    }
+                    priority.poll();
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        int p[] = {1, 1, 9, 1, 1, 1};
+        int l = 0;
+        int result = solution(p, l);
+        System.out.println(result);
+    }
+}
+
