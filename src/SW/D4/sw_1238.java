6059c3e373c580c094ef0f1b79ff41c97b0eb92d
BangKiHyun <rlrlvh@naver.com>
2019-08-01 15:52:04 +0900

    bfs응용

--- src/SW/D4/sw_1238.java
+++ src/SW/D4/sw_1238.java
@@ -10,17 +10,19 @@ public class sw_1238 {
     static LinkedList<Integer> a[];
     static Queue<Node> q;
     static int result[];
-    static int idx = 0;
+    static int max, temp;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int test_case = 1; test_case <= 1; test_case++) {
+        for (int test_case = 1; test_case <= 10; test_case++) {
             int len = sc.nextInt();
             int start = sc.nextInt();
             visit = new boolean[MAX];
             a = new LinkedList[MAX];
             q = new LinkedList<>();
             result = new int[MAX];
+            max = -1;
+            temp = 0;
             for (int i = 1; i < MAX; i++) {
                 a[i] = new LinkedList<>();
             }
@@ -29,28 +31,34 @@ public class sw_1238 {
                 int y = sc.nextInt();
                 a[x].add(y);
             }
-            q.add(new Node(start,1));
+            q.add(new Node(start, 0));
             visit[start] = true;
-            bfs();
+            int ans = result();
+            System.out.println("#" + test_case + " " + ans);
         }
     }
 
-    static void bfs() {
-        int max = 0;
+    static int result() {
+        int ans = 0;
         while (!q.isEmpty()) {
             Node n = q.poll();
-            if(max < n.cnt){
-
+            if (temp < n.cnt) {
+                temp = n.cnt;
+                max = n.num;
+            } else{
+                if(n.num > max){
+                    max = n.num;
+                }
             }
-            for (int i : a[x]) {
+            for (int i : a[n.num]) {
                 if (!visit[i]) {
-                    q.offer(i);
+                    q.add(new Node(i, n.cnt + 1));
                     visit[i] = true;
                 }
             }
-            idx++;
-            System.out.println(idx);
         }
+        ans = max;
+        return ans;
     }
 
     static class Node {
