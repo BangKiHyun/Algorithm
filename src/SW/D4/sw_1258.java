b48766c014b4a60b6b9fb0221b92c87a2bdcb715
BangKiHyun <rlrlvh@naver.com>
2019-08-06 14:28:35 +0900

    풀었다!

--- src/SW/D4/sw_1258.java
+++ src/SW/D4/sw_1258.java
@@ -6,6 +6,7 @@ public class sw_1258 {
     static int map[][];
     static ArrayList<Node> arr;
     static int a[];
+    static int ans[];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -14,8 +15,8 @@ public class sw_1258 {
             int n = sc.nextInt();
             map = new int[n + 1][n + 1];
             arr = new ArrayList<>();
-            a = new int[n];
-            int idx = 0;
+            a = new int[n+1];
+            ans = new int[n * 2];
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     map[i][j] = sc.nextInt();
@@ -27,14 +28,18 @@ public class sw_1258 {
                 int x = i;
                 for (int j = 0; j < n; j++) {
                     int y = j;
+                    int row = 0;
+                    int col = 0;
                     if (map[i][j] != 0) {
                         while (true) {
                             y++;
+                            row++;
                             if (map[i][y] == 0)
                                 break;
                         }
                         while (true) {
                             x++;
+                            col++;
                             if (map[x][j] == 0)
                                 break;
                         }
@@ -43,21 +48,43 @@ public class sw_1258 {
                                 map[k][l] = 0;
                             }
                         }
-                        arr.add(new Node(x, y, x * y));
+                        arr.add(new Node(row, col, row * col));
                     }
+                    x = i;
                     j = y;
                 }
             }
+            Collections.sort(arr);
+            System.out.print("#" + test_case + " " + arr.size());
+            for (Node s : arr) {
+                System.out.print(" " + s.y + " " + s.x);
+            }
+            System.out.println();
         }
     }
 
-    static class Node {
+    static class Node implements Comparable<Node> {
         int x, y, mul;
 
         public Node(int x, int y, int mul) {
             this.x = x;
             this.y = y;
             this.mul = mul;
+        }
+
+        @Override
+        public int compareTo(Node n) {
+            if (this.mul < n.mul) {
+                return -1;
+            } else if (this.mul > n.mul) {
+                return 1;
+            } else if (this.mul == n.mul) {
+                if (this.x < n.x) {
+                    return -1;
+                } else
+                    return 1;
+            }
+            return 0;
         }
     }
 }
