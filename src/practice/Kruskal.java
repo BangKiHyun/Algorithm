9af17936ad0da03b0b22e6d3fc7642449d0e5cc1
BangKiHyun <rlrlvh@naver.com>
2019-09-09 16:57:10 +0900

    최소 비용 신장 트리

--- src/practice/Kruskal.java
+++ src/practice/Kruskal.java
@@ -1,7 +1,6 @@
 package practice;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 //최소비용 신장 트리
 public class Kruskal {
@@ -19,33 +18,69 @@ public class Kruskal {
             parent[b] = a;
     }
 
-    static int findParent(int parent[], int a, int b) {
+    static boolean findParent(int parent[], int a, int b) {
         a = getParent(parent, a);
         b = getParent(parent, b);
         if (a == b)
-            return 1;
+            return true;
         else
-            return 0;
+            return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = 7;
+        int m = 11;
 
-        int parent[] = new int[8];
-        for (int i = 1; i <= 7; i++) {
+        int parent[] = new int[n];
+        ArrayList<Node> list = new ArrayList<>();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
-        int arr[][] = new int[8][8];
-        for (int i = 0; i<7;i++) {
+
+        for (int i = 0; i < m; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int dis = sc.nextInt();
-            arr[a][b] = dis;
+            list.add(new Node(a, b, dis));
         }
-        Arrays.sort(arr);
 
-        for(int i=1;i<=7;i++){
+        Collections.sort(list, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                if (o1.d < o2.d) {
+                    return -1;
+                } else if (o1.d > o2.d) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
 
+        for (int i = 0; i < m; i++) {
+            if (!findParent(parent, list.get(i).node[0] - 1, list.get(i).node[1] - 1)) {
+                sum += list.get(i).d;
+                System.out.print(list.get(i).d + " ");
+                unionParent(parent, list.get(i).node[0] - 1, list.get(i).node[1] - 1);
+            }
+        }
+        System.out.println(sum);
+    }
+
+    static class Node {
+        int node[] = new int[2];
+        int d;
+
+        Node(int a, int b, int d) {
+            this.node[0] = a;
+            this.node[1] = b;
+            this.d = d;
+        }
+
+        boolean op(Node n) {
+            return this.d < n.d;
         }
     }
 }
