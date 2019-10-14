23627e4645219c8cee76728657d27c107fe1189b
BangKiHyun <rlrlvh@naver.com>
2019-10-14 15:09:00 +0900

    벽 부수고 이동하기 시간줄이기

--- src/baekJ/bj_2206_BFS.java
+++ src/baekJ/bj_2206_BFS.java
@@ -1,6 +1,5 @@
 package baekJ;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -11,8 +10,8 @@ public class bj_2206_BFS {
     private static int[] dy = {0, 0, -1, 1};
     private static Queue<Node> q = new LinkedList<>();
     private static boolean visit[][];
-    private static int[][] copy;
     private static int min = Integer.MAX_VALUE;
+    private static boolean flag = false;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,85 +19,70 @@ public class bj_2206_BFS {
         int m = sc.nextInt();
         map = new int[n][m];
         visit = new boolean[n][m];
-        copy = new int[n][m];
 
         for (int i = 0; i < n; i++) {
+            String line = sc.next();
+            String[] row = line.split("");
             for (int j = 0; j < m; j++) {
-                map[i][j] = sc.nextInt();
+                map[i][j] = Integer.parseInt(row[j]);
             }
         }
+        solution(n, m);
 
-        q.add(new Node(0, 0));
-        visit[0][0] = true;
-        int path = findPath(n, m);
-        min = path == 0 ? min : path;
-
-        solution(n, m, 0, 0);
-    }
-
-    private static void solution(int n, int m, int start, int depth) {
-
-        if (depth == 1) {
-            Copy(n, m);
-            Arrays.fill(visit, false);
-            q.add(new Node(0, 0));
-            visit[0][0] = true;
-            int path = findPath(n, m);
-            if (path != 0) {
-                min = Math.min(min, path);
-                return;
-            } else return;
-        }
-
-        for (int i = start; i < n * m; i++) {
+        for (int i = 0; i < n * m; i++) {
             int x = i / n;
             int y = i % m;
             if (map[x][y] == 1) {
                 map[x][y] = 0;
-                solution(n, m, start + 1, depth + 1);
+                solution(n, m);
                 map[x][y] = 1;
             }
+        }
+
+        System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+    }
+
+    private static void solution(int n, int m) {
+        visit = new boolean[n][m];
+        q.add(new Node(0, 0, 1));
+        visit[0][0] = true;
+
+        int path = findPath(n, m);
+        if (flag) {
+            min = Math.min(min, path);
+            q.clear();
         }
     }
 
     private static int findPath(int n, int m) {
-        int cnt = 0;
-        boolean flag = false;
+        flag = false;
         while (!q.isEmpty()) {
             Node now = q.poll();
             if (now.x == n - 1 && now.y == m - 1) {
                 flag = true;
-                return cnt;
+                return now.cnt;
             }
 
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
 
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visit[nx][ny] && copy[nx][ny] == 0) {
-                    q.add(new Node(nx, ny));
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visit[nx][ny] && map[nx][ny] == 0) {
+                    q.add(new Node(nx, ny, now.cnt + 1));
                     visit[nx][ny] = true;
                 }
             }
-            cnt++;
         }
-        return cnt;
-    }
-
-    private static void Copy(int n, int m) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                copy[i][j] = map[i][j];
-            }
-        }
+        return -1;
     }
 
     private static class Node {
-        int x, y;
+        int x, y, cnt;
 
-        Node(int x, int y) {
+        Node(int x, int y, int cnt) {
             this.x = x;
             this.y = y;
+            this.cnt = cnt;
         }
     }
 }
