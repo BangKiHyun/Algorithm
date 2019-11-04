0445c73c9b5f8789af4a5f5d007fd9c61e08e959
BangKiHyun <rlrlvh@naver.com>
2019-11-04 18:57:00 +0900

    다른 풀이

--- src/programmers/pg_42577_Re.java
+++ src/programmers/pg_42577_Re.java
@@ -2,9 +2,9 @@ package programmers;
 
 import java.util.Arrays;
 
-public class pg_42577 {
+public class pg_42577_Re {
     static public boolean solution(String[] phone_book) {
-        boolean answer = true;
+        /*boolean answer = true;
         int[] post = new int[phone_book.length];
         for (int i = 0;i<phone_book.length;i++){
             post[i] = Integer.parseInt(phone_book[i]);
@@ -20,8 +20,17 @@ public class pg_42577 {
                     return false;
                 }
             }
+        }*/
+
+        boolean flag = true;
+        for(int i=0; i<phone_book.length;i++) {
+            for(int j=0; j<phone_book.length; j++) {
+                if(!phone_book[i].equals(phone_book[j])&&phone_book[i].startsWith(phone_book[j])) {
+                    return false;
+                }
+            }
         }
-        return answer;
+        return flag;
     }
 
     public static void main(String[] args){
